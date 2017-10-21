16d039730bde3f4ec8dbaa90d8deea1496c6eef8
JacqueWhite <jacquecwhite@gmail.com>
2017-10-21 16:37:49 -0500

    adding meta tags and fixing emailer

--- sendEmail.php
+++ sendEmail.php
@@ -1,8 +1,6 @@
 <?php
 
-// Replace this with your own email address
 $siteOwnersEmail = 'jacquecwhite@gmail.com';
-
 
 if($_POST) {
 
@@ -20,16 +18,16 @@ if($_POST) {
 		$error['email'] = "Please enter a valid email address.";
 	}
 	// Check Message
-	if (strlen($contact_message) < 5) {
-		$error['message'] = "Please enter your message. It should have at least 15 characters.";
+	if (strlen($contact_message) < 4) {
+		$error['message'] = "Please enter your message. It should have at least 4 characters.";
 	}
    // Subject
 	if ($subject == '') { $subject = "Contact Form Submission"; }
 
 
    // Set Message
-   $message .= "Email from: " . $name . "<br />";
-	$message .= "Email address: " . $email . "<br />";
+   $message = "Email from: " . $name . "<br />";
+   $message .= "Email address: " . $email . "<br />";
    $message .= "Message: <br />";
    $message .= $contact_message;
    $message .= "<br /> ----- <br /> This email was sent from your site's contact form. <br />";
@@ -38,7 +36,7 @@ if($_POST) {
    $from .=  $name . " <" . $email . ">";
 
    // Email Headers
-	$headers .= "From: " . $from . "\r\n";
+	$headers = "From: " . $from . "\r\n";
 	$headers .= "Reply-To: ". $email . "\r\n";
  	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
@@ -56,7 +54,7 @@ if($_POST) {
 
 	else {
 
-		$response .= (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
+		$response = (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
 		$response .= (isset($error['email'])) ? $error['email'] . "<br /> \n" : null;
 		$response .= (isset($error['message'])) ? $error['message'] . "<br />" : null;
 		
